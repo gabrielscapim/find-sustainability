e07c8153dbc8b2df9585427ef4b960a86540ff60
Gabriel <scapimgabriel@gmail.com>
2023-07-26 20:56:50 -0300

    style: add font style

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="header-container">
     <nav>
       <NuxtLink to="/">Logo</NuxtLink>
       <ul>
@@ -19,6 +19,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .header-container {
+    border: 1px solid red;
+    font-family: 'MuseoSansRounded-300';
+  }
 </style>
