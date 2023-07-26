6f4367ae5171bc3b1d5348cc85d0f82eb894d696
Gabriel <scapimgabriel@gmail.com>
2023-07-26 20:48:11 -0300

    feat: created header component and layouts page

---
+++ frontend/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav>
+      <NuxtLink to="/">Logo</NuxtLink>
+      <ul>
+        <li>
+          <NuxtLink to="/">Buscar Startup</NuxtLink>
+          <NuxtLink to="/">Cadastrar Startup</NuxtLink>
+          <NuxtLink to="/">Sobre</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
