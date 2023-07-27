52d3c4eed2e3eac04de64f9c3ac877ec286dc847
Gabriel <scapimgabriel@gmail.com>
2023-07-27 16:22:32 -0300

    feat: router extras package and create add startup page

---
+++ frontend/pages/AddStartup.vue
@@ -0,0 +1,21 @@
+<router>
+  {
+    path: '/add-startup'
+  }
+</router>
+
+<template>
+  <p>
+    Hello world
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
