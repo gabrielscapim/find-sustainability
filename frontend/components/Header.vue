3177f8838d68e08b18a082e6d99498ee5570b67e
Gabriel <scapimgabriel@gmail.com>
2023-07-27 09:24:25 -0300

    style: header

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -5,9 +5,9 @@
         <a href="/">Logo</a>
       </div>
       <div class="header-line-2">
-        <a href="/">Buscar Startup</a>
-        <a href="/">Cadastrar Startup</a>
-        <a href="/">Sobre</a>
+        <a class="header-option" href="/">Sobre</a>
+        <a class="header-option" href="/">Buscar Startup</a>
+        <a class="header-option" href="/">Cadastrar Startup</a>
       </div>
     </nav>
   </header>
@@ -20,5 +20,53 @@ export default {
 </script>
 
 <style scoped>
-  
+  .header-container {
+    padding: 24px 20px 24px 20px;
+  }
+
+  .header-line-2 {
+    margin-left: 10%;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .header-container nav {
+    display: flex;
+  }
+
+  .header-option {
+    color: black;
+    text-decoration: none;
+    font-weight: 600;
+    margin-right: 5%;
+  }
+
+  .header-option:hover {
+    color: #6A0FB2;
+  }
+
+  @media all and (max-width: 768px) {
+    .header-container nav {
+      flex-direction: column;
+    }
+
+    .header-line-1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .header-line-2 {
+      margin-left: 0%;
+      margin-top: 24px;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      overflow: auto;
+      white-space: nowrap;
+      overflow-x: scroll;
+    }
+
+}
 </style>
