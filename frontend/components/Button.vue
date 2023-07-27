45ab64837f8d9b4fac97128edb5d4a21fec8c8f9
Gabriel <scapimgabriel@gmail.com>
2023-07-27 16:03:59 -0300

    feat: button component

---
+++ frontend/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    type="button"
+    @click="$emit('handleClick')"
+    class="button"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+  }
+}
+</script>
+
+<style>
+  .button {
+    font-family: 'MuseoSansRounded-300';
+    background-color: #91B31E;
+    border: 1px solid #90b31ea8;
+    padding: 12px 18px 12px 18px;
+    border-radius: 4px;
+    color: white;
+    font-size: 20px;
+  }
+
+  .button:hover {
+    background-color: #1A614F;
+    border: 1px solid #1A614F;
+    cursor: pointer;
+  }
+
+</style>
