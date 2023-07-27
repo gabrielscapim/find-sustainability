020d214abdc06a72b01927a91118ed5dae80de94
Gabriel <scapimgabriel@gmail.com>
2023-07-27 09:40:23 -0300

    feat: main image one

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>Hello world</p>
+  <section class="main-section-1">
+    <img
+      src="../static/images/main-image-1.jpg"
+      alt="main-1"
+      class="main-image-1"
+    >
+  </section>
 </template>
 
 <script>
@@ -7,3 +13,14 @@ export default {
   name: ''
 }
 </script>
+
+<style scoped>
+  .main-seciton-1 {
+    width: 100%;
+    display: flex;
+  }
+
+  .main-image-1 {
+    width: 50%;
+  }
+</style>
