49f61c50a4e2f291262689c172cc75f509153202
Gabriel <scapimgabriel@gmail.com>
2023-07-27 10:19:42 -0300

    feat: main page image and slogan

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <section class="main-section-1">
-    <img
-      src="../static/images/main-image-1.jpg"
-      alt="main-1"
-      class="main-image-1"
-    >
-  </section>
+  <main class="main-container">
+    <article class="main-line-1-column-1">
+      <h1>
+        A <span>sustentabilidade</span> está a um clique
+      </h1>
+      <p>
+        Explore nossa plataforma e descubra startups comprometidas em construir um futuro melhor para todos. Cada projeto é um elo valioso na preservação do nosso planeta.
+      </p>
+    </article>
+    <div class="main-line-1-column-2">
+      <img
+        src="../static/images/main-image-1.jpg"
+        alt="main-1"
+        class="main-image-1"
+      >
+    </div>
+  </main>
+      
 </template>
 
 <script>
@@ -15,12 +26,43 @@ export default {
 </script>
 
 <style scoped>
-  .main-seciton-1 {
-    width: 100%;
+  .main-container {
     display: flex;
+    width: 100%;
   }
 
-  .main-image-1 {
+  .main-line-1-column-1 {
     width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
   }
+
+
+  .main-line-1-column-1 h1 {
+    font-size: 32px;
+    margin-bottom: 48px;
+  }
+
+  .main-line-1-column-1 span {
+    color: #91B31E;
+  }
+
+  .main-line-1-column-1 p {
+    font-size: 24px;
+  }
+
+  .main-line-1-column-2 {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .main-line-1-column-2 img {
+    width: 90%;
+  }
+
 </style>
