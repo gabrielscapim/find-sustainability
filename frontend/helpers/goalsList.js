ae6473d0fb1ab0afaefb0d1fcec04b8e0b01ba1a
Gabriel <scapimgabriel@gmail.com>
2023-07-28 18:40:25 -0300

    feat: goals list to array of objects

--- frontend/helpers/goalsList.js
+++ frontend/helpers/goalsList.js
@@ -1,21 +1,21 @@
 const goalsList = [
-  "1. Erradicação da Pobreza",
-  "2. Fome Zero e Agricultura Sustentável",
-  "3. Saúde e Bem-Estar",
-  "4. Educação de Qualidade",
-  "5. Igualdade de Gênero",
-  "6. Água Potável e Saneamento",
-  "7. Energia Limpa e Acessível",
-  "8. Trabalho Decente e Crescimento Econômico",
-  "9. Indústria, Inovação e Infraestrutura",
-  "10. Redução das Desigualdades",
-  "11. Cidades e Comunidades Sustentáveis",
-  "12. Consumo e Produção Responsáveis",
-  "13. Ação Contra a Mudança Global do Clima",
-  "14. Vida na Água",
-  "15. Vida Terrestre",
-  "16. Paz, Justiça e Instituições Eficazes",
-  "17. Parcerias e Meios de Implementação"
+  { name: '1. Erradicação da Pobreza', selected: false },
+  { name: '2. Fome Zero e Agricultura Sustentável', selected: false },
+  { name: '3. Saúde e Bem-Estar', selected: false },
+  { name: '4. Educação de Qualidade', selected: false },
+  { name: '5. Igualdade de Gênero', selected: false },
+  { name: '6. Água Potável e Saneamento', selected: false },
+  { name: '7. Energia Limpa e Acessível', selected: false },
+  { name: '8. Trabalho Decente e Crescimento Econômico',selected: false },
+  { name: '9. Indústria, Inovação e Infraestrutura', selected: false },
+  { name: '10. Redução das Desigualdades', selected: false },
+  { name: '11. Cidades e Comunidades Sustentáveis', selected: false },
+  { name: '12. Consumo e Produção Responsáveis', selected: false },
+  { name: '13. Ação Contra a Mudança Global do Clima', selected: false},
+  { name: '14. Vida na Água', selected: false },
+  { name: '15. Vida Terrestre', selected: false },
+  { name: '16. Paz, Justiça e Instituições Eficazes', selected: false },
+  { name: '17. Parcerias e Meios de Implementação', selected: false }
 ];
 
 export default goalsList;
