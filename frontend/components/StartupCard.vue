507a0247e46f0d8e360bd32e3cc79adb69ac2c32
Gabriel <scapimgabriel@gmail.com>
2023-07-29 12:58:39 -0300

    style: startup card

--- frontend/components/StartupCard.vue
+++ frontend/components/StartupCard.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="card-container">
-    <img :src="src" :alt="title" >
-    <div>
+    <img
+      :src="src"
+      :alt="title"
+      class="image"
+    >
+    <div class="info-container">
         <h3>{{ title }}</h3>
         <p>{{ description }}</p>
     </div>
@@ -35,6 +39,42 @@ export default {
 
 <style>
   .card-container {
-    border: 1px solid red;
+    border: 1px solid #e6e5e1;
+    width: 350px;
+    padding: 16px;
+    border-radius: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 1%;
+    margin-bottom: 36px;
+  }
+
+  .info-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .card-container h3 {
+    width: 70%;
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .card-container p {
+    margin: 16px 16px 32px 16px;
+    line-height: 1.5;
+    text-align: justify;
+  }
+
+  .card-container button {
+    font-size: 14px;
+  }
+
+  .image {
+    max-width: 200px;
   }
 </style>
--- frontend/pages/FindStartup.vue
+++ frontend/pages/FindStartup.vue
@@ -6,11 +6,29 @@
 
 
 <template>
-  <StartupCard 
-    title="Nome da empresa"
-    description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
-    src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
-  />
+  <section>
+    <StartupCard 
+      title="Nome da empresa"
+      description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
+      src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
+    />
+    <StartupCard 
+      title="Nome da empresa"
+      description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
+      src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
+    />
+    <StartupCard 
+      title="Nome da empresa"
+      description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
+      src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
+    />
+    <StartupCard 
+      title="Nome da empresa"
+      description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
+      src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
+    />
+  </section>
+  
 </template>
 
 <script>
@@ -24,5 +42,9 @@ export default {
 </script>
 
 <style>
-
+section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
 </style>
