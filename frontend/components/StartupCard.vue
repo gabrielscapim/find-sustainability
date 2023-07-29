59f6a681da9cf6effeb24a587d190db0fe5bcb77
Gabriel <scapimgabriel@gmail.com>
2023-07-29 12:18:53 -0300

    feat: startup card

---
+++ frontend/components/StartupCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-container">
+    <img :src="src" :alt="title" >
+    <div>
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
+    </div>
+    <Button label="Saiba mais" />
+  </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  components: {
+    Button,
+  },
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style>
+  .card-container {
+    border: 1px solid red;
+  }
+</style>
--- frontend/pages/FindStartup.vue
+++ frontend/pages/FindStartup.vue
@@ -6,12 +6,20 @@
 
 
 <template>
-  
+  <StartupCard 
+    title="Nome da empresa"
+    description="Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Suspendisse sagittis."
+    src='https://api.freelogodesign.org/assets/thumb/logo/a17b07eb64d341ffb1e09392aa3a1698_400.png'
+  />
 </template>
 
 <script>
-export default {
+import StartupCard from '../components/StartupCard.vue'
 
+export default {
+  comments: {
+    StartupCard,
+  }
 }
 </script>
 
