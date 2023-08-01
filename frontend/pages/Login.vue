4e58d2477c25f7231f397dbd218cc763dd2c4000
Gabriel <scapimgabriel@gmail.com>
2023-08-01 11:28:03 -0300

    styles: login page

--- frontend/pages/Login.vue
+++ frontend/pages/Login.vue
@@ -7,7 +7,10 @@
 
 <template>
   <section class="page-container">
-    <form>
+    <h1 class="page-title">
+      Obrigado por escolher fazer a <span>diferença!</span>
+    </h1>
+    <form class="form">
       <Input
         id="input-email-login"
         label="E-mail"
@@ -52,5 +55,41 @@ export default {
 </script>
 
 <style>
- 
+  .page-container {
+    display: flex;
+    height: 90vh;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .page-container h1 {
+    text-align: center;
+  }
+
+  .page-container span {
+    color: #91B31E;
+  }
+
+  .form {
+    min-width: 350px;
+    max-width: 400px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid #e6e5e1;
+    border-radius: 12px;
+    padding: 20px;
+    height: 300px;
+    justify-content: center;
+    margin-top: 20px;
+  }
+
+  .button {
+    max-height: 48px;
+    max-width: 85px;
+  }
+
+  .input {
+    width: 100%;
+  }
 </style>
