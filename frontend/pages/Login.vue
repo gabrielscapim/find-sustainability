9d38e594d58b523879436e993fc9907d0df99738
Gabriel <scapimgabriel@gmail.com>
2023-08-01 11:13:34 -0300

    feat: login page

---
+++ frontend/pages/Login.vue
@@ -0,0 +1,56 @@
+<router>
+  {
+    path: '/login'
+  }
+</router>
+
+
+<template>
+  <section class="page-container">
+    <form>
+      <Input
+        id="input-email-login"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        v-model="email"
+      />
+      <Input
+        id="input-passord-login"
+        label="Senha"
+        placeholder="Digite sua senha"
+        v-model="email"
+      />
+      <Button 
+        label="Entrar"
+        @handleClick="handleSubmit"
+      />
+    </form>
+  </section>
+</template>
+
+<script>
+import Button from '../components/Button.vue';
+import Input from '../components/Input.vue'
+
+export default {
+  components: {
+    Input,
+    Button,
+  },
+  methods: {
+    handleSubmit() {
+      console.log('clicou');
+    }
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  }
+}
+</script>
+
+<style>
+ 
+</style>
