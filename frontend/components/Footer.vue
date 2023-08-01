4b4d1f28538742032de62a0d3337940a33e45350
Gabriel <scapimgabriel@gmail.com>
2023-07-31 22:35:17 -0300

    feat: footer component

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer-container">
+    Developed by <a href="https://www.linkedin.com/in/gabrielscapim/">Gabriel Scapim</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .footer-container {
+    border: 1px solid red;
+  }
+</style>
