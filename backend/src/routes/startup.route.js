1d49888c9c606e6f7ab18e7224b6b2fa67f235bd
Gabriel <scapimgabriel@gmail.com>
2023-08-01 18:15:59 -0300

    feat: jwt auth in get startup by id endpoint

--- backend/src/routes/startup.route.js
+++ backend/src/routes/startup.route.js
@@ -4,7 +4,7 @@ const validateStartupEmail = require('../middlewares/validateStartupEmail');
 const validateStartupFields = require('../middlewares/validateStartupFields');
 const { validateToken } = require('../middlewares/validateToken');
 
-route.get('/:id', startupController.getStartupById);
+route.get('/:id', validateToken, startupController.getStartupById);
 route.delete('/:id', validateToken, startupController.deleteStartup);
 route.put('/:id', validateToken, startupController.editStartup);
 route.get('/search/goal', startupController.getStartupsByGoal);
