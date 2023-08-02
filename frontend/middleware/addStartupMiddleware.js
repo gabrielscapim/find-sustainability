d2d7b724049ad7d5d0443db93ff92c4260d161c8
Gabriel <scapimgabriel@gmail.com>
2023-08-02 09:07:45 -0300

    feat: add startup middleware

---
+++ frontend/middleware/addStartupMiddleware.js
@@ -0,0 +1,21 @@
+import resetLocalStorage from "../helpers/resetLocalStorage";
+import { apiRequest } from "../services/apiRequest";
+
+export default async ({ route, redirect }) => {
+  const startupId = route.query.id;
+
+  if (startupId) {
+    try {
+      const { email } = JSON.parse(localStorage.getItem('startup'));
+      const startupToEdit = await apiRequest('get', `/startup/${startupId}`);
+  
+      if (email !== startupToEdit.email) {
+        resetLocalStorage();
+        return redirect('login');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+  
