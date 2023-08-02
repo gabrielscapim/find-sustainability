1b032e1130e3c0088796fba5ba8e50244ea2ccea
Gabriel <scapimgabriel@gmail.com>
2023-08-02 09:03:44 -0300

    feat: auth middleware for login

--- frontend/middleware/authMiddleware.js
+++ frontend/middleware/authMiddleware.js
@@ -1,13 +1,17 @@
-export default ({ route, redirect }) => {
-  try {
-    const startupId = localStorage.getItem('startup_id');
-    const token = localStorage.getItem('token');
+import resetLocalStorage from "../helpers/resetLocalStorage";
 
-    if (startupId && !route.query.id && token) {
-      return redirect(`/add-startup?id=1`);
-    }
-    
-  } catch (error) {
-    console.log(error);
+export default ({ redirect }) => {
+  const startup = JSON.parse(localStorage.getItem('startup'));
+  const token = localStorage.getItem('token');
+  const tokenGenerationTime = localStorage.getItem('tokenGenerationTime');
+  const dateNow = Date.now();
+  const TOKEN_EXPIRATION_TIME = 86400000;
+
+  if (startup && token && (dateNow - tokenGenerationTime < TOKEN_EXPIRATION_TIME)) {
+    redirect(`/add-startup?id=${startup.id}`)
+  }
+
+  if (dateNow - tokenGenerationTime > TOKEN_EXPIRATION_TIME) {
+    resetLocalStorage();
   }
 }
