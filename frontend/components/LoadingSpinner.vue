e5c1c9e8f8a87198ff25a854d00a9512fd59afee
Gabriel <scapimgabriel@gmail.com>
2023-08-02 09:44:19 -0300

    styles: loading component

--- frontend/components/LoadingSpinner.vue
+++ frontend/components/LoadingSpinner.vue
@@ -5,24 +5,23 @@
 </template>
 
 <style scoped>
-    .loading-spinner {
+  .loading-spinner {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
-    }
+  }
 
-    .spinner {
-    width: 50px;
-    height: 50px;
+  .spinner {
+    width: 25px;
+    height: 25px;
     border: 5px solid #f3f3f3;
-    border-top: 5px solid #3498db;
+    border-top: 5px solid #91B31E;
     border-radius: 50%;
     animation: spin 1s linear infinite;
-    }
+  }
 
-    @keyframes spin {
+  @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
-    }
+  }
 </style>
